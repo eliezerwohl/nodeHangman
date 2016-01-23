bbd065f218035c46447b995285b3a5f44f1e29ee
eliezerwohl <eliezerwohl@gmail.com>
2016-01-23 16:38:33 -0500

    added and created letter.js file

---
+++ word.js
@@ -0,0 +1,13 @@
+var Letter = require("./letter")
+word("poop");
+
+function word(wrd){
+  this.word = wrd;
+  this.lets = [];
+  this.found = false;
+  console.log()
+  this.getLets = function(){
+    
+  }
+
+}
