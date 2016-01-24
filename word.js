54deb65d4efa6cfd7e6f55630e86fdb75fe35009
eliezerwohl <eliezerwohl@gmail.com>
2016-01-23 19:05:38 -0500

    started word.js for homework

--- word.js
+++ word.js
@@ -1,13 +1,25 @@
 var Letter = require("./letter")
 word("poop");
-
+checkIfLetterFound("p")
 function word(wrd){
   this.word = wrd;
   this.lets = [];
   this.found = false;
   console.log()
   this.getLets = function(){
-    
+    for (var i = 0; i < this.word.length; i++) {
+      var letter = this.word[i];
+      this.lets.push(letter)
+    };
+   
+  }
+  this.checkIfLetterFound = function(guessLetter){
+    var whatToReturn = 0;
+    for (var i = 0; i < this.lets.length; i++) {
+      if (this.lets[i] = Letter.charac)  {
+        
+      }
+    };
   }
 
 }
