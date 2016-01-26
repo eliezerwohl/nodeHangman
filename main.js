89ce75139571b98233b79c6378cb0d4dc529005c
eliezerwohl <eliezerwohl@gmail.com>
2016-01-26 14:41:49 -0500

    starting to fix bugs.  forgot ) after using the prompt npm.

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-var Word = require("./word.js")  
+var Word = require("./word.js");  
 var prompt = require('prompt');
-
-prompt.start()
 
 var game = {
   wordbank : ["motorhead", "sabbath", "negative", "scratch", "antidisestablishmentarianism"],
   guessesRemaining : 10,
   currentWord : null,
-  startgame : function(wrd){
-    this.currentWord = new Word[Math.floor(Math.random() * this.wordbank.length)]
+  startGame : function(wrd){
+    this.currentWord = new Word([Math.floor(Math.random() * this.wordbank.length)])
     this.currentWord.getLets();
     this.keepPromptingUser();
-  }
+  },
   keepPromptingUser : function(){
     var self = this;
-     prompt.start();
-     prompt.get(['Guess letter friendo!'], function (err, result) {  
+    prompt.start();
+    prompt.get(['Guess letter friendo!'], function (err, result) {  
       // friendo is a homestarrunner refrence
       console.log("The Letter or space you guessed is" + result.guessLetter);
       var findHowManyOfUserGuess = currentWrd.checkIfLetterFound(result.guessLetter);
@@ -26,24 +24,25 @@ var game = {
       }
       else{
         console.log("you guessed correctly!  here's a gold star!")
-          if (currentWrd.didWeFindTheWord() = true){
-            console.log("you won the game! also, this game works!")
-            return true
-          }
+        if (currentWrd.didWeFindTheWord() = true){
+          console.log("you won the game! also, this game works!")
+          return true
         }
-      console.log("guessing remaining" = guessesRemaining.val())
-      console.log(wordRender(currentWrd))
-      if ((guessesRemaining > 0) && (currentWrd.found === false)){
+      }
+    });
+    console.log("guessing remaining" + game.guessesRemaining.val)
+    console.log(wordRender(currentWrd))
+    if ((game.guessesRemaining > 0) && (currentWrd.found === false)){
         keepPromptingUser();
-      }
-      else if (guessesRemaining === 0){
-        console.log(game over bruh);
-        console.log(this.currentWord);
-      }
-      else{
-        console.log(wordRender(currentWrd))
-      }
     }
-  }
-}
+    else if (guessesRemaining === 0){
+      console.log("game over bruh");
+      console.log(this.currentWord);
+    }
+    else{
+      console.log(wordRender(currentWrd))
+    }
+  },   
+}  //end start
 game.startGame()
+
